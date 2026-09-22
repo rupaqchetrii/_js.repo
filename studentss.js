90587a0019046f374ce7ac33b81610956e87ab87
rupaqchetrii <rchetry51123@gmail.com>
2026-09-22 17:04:02 +0530

    simple use of data types

---
+++ studentss.js
@@ -0,0 +1,26 @@
+var studentName = 'RUPAK CHETRY';
+let age = 18;
+const course = 'BTECH';
+var collegeName = 'SITM';
+let marks = 98;
+const birthYear = 2008;
+let city = 'GUWAHATI';
+let semester = 1;
+let favouriteSubject = 'JavaScript';
+
+studentName = 'Rupak Chettry';
+age = 19;
+marks = 99;
+city = 'Shillong';
+semester = 2;
+
+
+console.log('Student Name:', studentName);
+console.log('Age:', age);
+console.log('Course:', course);
+console.log('College Name:', collegeName);
+console.log('Marks:', marks);
+console.log('Birth Year:', birthYear);
+console.log('City:', city);
+console.log('Semester:', semester);
+console.log('Favourite Subject:', favouriteSubject);
